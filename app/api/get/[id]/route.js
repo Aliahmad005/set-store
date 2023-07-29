c19a73b9dc4c397ce9e27f70e16b18ffbf670927
Aliahmad005 <aliahmad051999@gmail.com>
2023-07-29 19:51:08 +0500

    backend

--- app/api/get/[id]/route.js
+++ app/api/get/[id]/route.js
@@ -9,10 +9,34 @@ import { NextResponse } from "next/server";
 
 
 
-export async function GET(request, { params }) {
-    const { id } = params
-    console.log('parms' , id)
-    await connectDB();
-    const topic = await Product.findOne({ _id: id });
-    return NextResponse.json({ topic }, { status: 200 });
+// export async function GET(request, { params }) {
+//     const { id } = params
+//     console.log('parms' , id)
+//     await connectDB();
+//     const topic = await Product.findOne({ _id: id });
+//     return NextResponse.json({ topic }, { status: 200 });
+//   }
+
+
+
+  export async function GET(request, {params}){
+
+try{
+  if(!params || !params.id){
+    throw new Error("missing product id")
+  }
+  const {id} = params
+  await connectDB();
+  const topic = await Product.findOne({ _id: id });
+  if(!topic){
+    throw new Error("topic dont finde")
+  }
+return NextResponse.json({topic}, {status:200})
+}catch (error){
+console.error(error)
+return NextResponse.json({error : error.message} ,{status : 400})
+}
+
+
+
   }
