f963305bf8286b567aa9eb4bada101b7ff6d4882
Aliahmad005 <aliahmad051999@gmail.com>
2023-07-29 20:54:44 +0500

    fix some issue

--- app/api/get/[id]/route.js
+++ app/api/get/[id]/route.js
@@ -9,14 +9,6 @@ import { NextResponse } from "next/server";
 
 
 
-// export async function GET(request, { params }) {
-//     const { id } = params
-//     console.log('parms' , id)
-//     await connectDB();
-//     const topic = await Product.findOne({ _id: id });
-//     return NextResponse.json({ topic }, { status: 200 });
-//   }
-
 
 
   export async function GET(request, {params}){
@@ -25,10 +17,9 @@ try{
   if(!params || !params.id){
     throw new Error("missing product id")
   }
-  const id = params.id
-  console.log("id", id)
+
   await connectDB();
-  const topic = await Product.findOne({ _id: id });
+  const topic = await Product.findOne({ _id: params.id});
   if(!topic){
     throw new Error("topic dont finde")
   }
