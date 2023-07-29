c67e7ca15b24f22e01207c7693456d555edb48d3
Aliahmad005 <aliahmad051999@gmail.com>
2023-07-29 21:32:10 +0500

    resolve

--- app/api/getId/route.js
+++ app/api/getId/route.js
@@ -5,21 +5,3 @@ import connectDB from "../db";
 import Product from "../product";
 import { NextResponse } from "next/server";
 
-
-
-
-//   export async function GET(request, { params }) {
-//     const { id } = params;
-//     console.log("server id", id)
-//     await connectDB();
-//     const topic = await Product.findOne({ _id: id });
-//     return NextResponse.json({ topic }, { status: 200 });
-//   }
-
-export async function GET(request, { params }) {
-    const { id } = params
-    console.log('parms' , params)
-    await connectDB();
-    const topic = await Product.findOne({ _id: id });
-    return NextResponse.json({ topic }, { status: 200 });
-  }
